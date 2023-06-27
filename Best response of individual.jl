dc3fc6a479f3399a2a967d6176a78dd9e8aa3b69
Linh <linh.nguyen@ens-paris-saclay.fr>
2023-06-27 12:27:45 +0200

    Update and rename areaworking.jl to Best response of individual.jl

--- Best response of individual.jl
+++ Best response of individual.jl
@@ -1,6 +1,6 @@
 ##################### BEST RESPONSE VS. FULL INFO ##################
 ####################################################################
-##### ∃ α, ϕ, c #### BENCHMARK CASE ################################
+#################### BENCHMARK CASE ################################
 ####################################################################
 using Plots
 
@@ -20,7 +20,7 @@ function h(x, g, d, s, α)
     end
 end
 
-d = 0.25 #keep it instead of full-filled
+d = 0.25 
 g = 0.5
 s = 0.3
 c = 0.2
@@ -44,16 +44,9 @@ label="crop without pesticide use")
 
 hline!([c/ϕ], linestyle=:dash, linecolor=:black, label="")
 vline!([c/ϕ], linestyle=:dash, linecolor=:black, label="")
-### legend position for case 1:
+
 plot!(legend=:outerbottom)
 
-#for 45 degree line:
-x_line = collect(range(0, 1, length=100))
-y_line = x_line
-plot!(x_line, y_line, linestyle=:solid, linecolor=:blue, label="")
-
-# save
-savefig(fig,"fullinfo.png")
 
 ## END benchmark case of full ecological info ##############
 ############################################################
@@ -108,56 +101,6 @@ vline!([c/ϕ], linestyle=:dash, linecolor=:black, label="")
 
 plot!(legend=:bottom)
 
-#for 45 degree line:
-x_line = collect(range(0, 1, length=100))
-y_line = x_line
-plot!(x_line, y_line, linestyle=:solid, linecolor=:blue, label="")
-
-# save
-savefig(fig,"sensitive4.png")
-
 ###########################################
 ## END OF SENSITIVE ANALYSIS ##############
 ############################################################
-
-
-
-
-### legend position:
-plot!(legend=:outerleft)
-plot!(legend=:outerbottomleft)
-plot!(legend=:bottom)
-
-########################
-##### plot for my neighbor j: another test thui khong co gi dau dung post 
-########################
-Y = 0:0.01:1
-
-function a(y, g, d, π, c, ϕ, α)
-    if y >= c/ϕ
-        return (1/d)*(1 - π - c) - (1/d)*(1 + g - d - ϕ - (α/2))*y + (g/d)*y^2
-    else
-        return NaN
-    end
-end
-
-function b(y, g, d, π, α)
-    if y <= c/ϕ
-        return (1/d)*(1 - π) - (1/d)*(1 + g - d - (α/2))*y + (g/d)*y^2
-    else
-        return NaN
-    end
-end
-fig = plot(xlabel="Pi", ylabel="Pj", xlims=(0,1), ylims=(0, 1), 
-title="Case 2: test j", 
-aspect_ratio=:equal)
-
-plot!(Y, a.(Y, g, d, π, c, ϕ, α), fill=(0, :pink), alpha=0.5, 
-label="∃ both ϕ, α")
-
-plot!(Y, b.(Y, g, d, π, α), fill=(0, :yellow), alpha=0.5, 
-label="∃ only α")
-hline!([c/ϕ], linestyle=:dash, label="c/ϕ")
-
-#save fig
-savefig(fig,"Case2Main.png") 
